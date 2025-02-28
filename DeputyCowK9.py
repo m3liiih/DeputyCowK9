3ba79b78015dca9024f36f8da62876e8d7d86660
m3liiih <kivancahmetmelih@gmail.com>
2025-03-01 01:45:21 +0300

    win conditions and results added

--- DeputyCowK9.py
+++ DeputyCowK9.py
@@ -23,3 +23,52 @@ def check_input(player_input):
 
 def computer_turn(move_options = ["K-9", "Deputy", "Cow"]):
     computer_move = random.choice(move_options)
+    return computer_move
+
+def results(player_move, computer_move):
+    if player_move == "K-9":
+        if computer_move == "Cow":
+            player_win = 1
+            comp_win = 0
+            print("Computer chose Cow. You Won!")
+        elif computer_move == "Deputy":
+            player_win = 0
+            comp_win = 1
+            print("Computer chose Deputy. You Lost!")
+        else:
+            player_win = 0
+            comp_win = 0
+            print("Computer chose K-9. Draw!")
+    
+    if player_move == "Deputy":
+        if computer_move == "K-9":
+            player_win = 1
+            comp_win = 0
+            print("Computer chose K-9. You Won!")
+        elif computer_move == "Cow":
+            player_win = 0
+            comp_win = 1
+            print("Computer chose Cow. You Lost!")
+        else:
+            player_win = 0
+            comp_win = 0
+            print("Computer chose Deputy. Draw!")
+
+    if player_move == "Cow":
+        if computer_move == "Deputy":
+            player_win = 1
+            comp_win = 0
+            print("Computer chose Deputy. You Won!")
+        elif computer_move == "K-9":
+            player_win = 0
+            comp_win = 1
+            print("Computer chose K-9. You Lost!")
+        else:
+            player_win = 0
+            comp_win = 0
+            print("Computer chose Cow. Draw!")
+
+player_input = game_intro()
+computer_move = computer_turn()
+player_move = check_input(player_input)
+results(player_move, computer_move)
