b0ea8b1eeca71125dcb81cdb8080e2fcf8416432
m3liiih <kivancahmetmelih@gmail.com>
2025-02-28 23:57:15 +0300

    new_round func with global var for reuse

--- DeputyCowK9.py
+++ DeputyCowK9.py
@@ -1,5 +1,11 @@
 import random
 
+def new_round():
+    print("Select K-9/Deputy/Cow or K/D/C:")
+    global player_input
+    player_input = input("-- ")
+    return player_input
+
 def game_intro():
     print("K-9 bites Cow, Deputy tases K-9, Cow kicks Deputy")
     new_round()
