5f17a388863df91ad64e3e30b074933487ec14d7
m3liiih <kivancahmetmelih@gmail.com>
2025-03-01 01:04:46 +0300

    various fixes

--- DeputyCowK9.py
+++ DeputyCowK9.py
@@ -2,25 +2,24 @@ import random
 
 def new_round():
     print("Select K-9/Deputy/Cow or K/D/C:")
-    global player_input
     player_input = input("-- ").upper()
     return player_input
 
 def game_intro():
     print("K-9 bites Cow, Deputy tases K-9, Cow kicks Deputy")
-    new_round()
-game_intro()
+    return new_round()
 
-def check_input(player_move):
-    if player_move in ["K", "K-9", "K9"]:
+def check_input(player_input):
+    if player_input in ["K", "K-9", "K9"]:
         player_move = "K-9"
-    elif player_move in ["D", "DEPUTY"]:
+    elif player_input in ["D", "DEPUTY"]:
         player_move = "Deputy"
-    elif player_move in ["C", "COW"]:
+    elif player_input in ["C", "COW"]:
         player_move = "Cow"
     else:
         print("Unregistered input. Please try again...\n")
         new_round()
+    return player_move
 
 def computer_turn(move_options = ["K-9", "Deputy", "Cow"]):
     computer_move = random.choice(move_options)
